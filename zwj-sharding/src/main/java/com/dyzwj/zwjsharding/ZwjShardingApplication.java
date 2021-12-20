06b62452a2e3129b971d2410fdbe86c6b57d6a4d
unknown <YC02167@mbcoffice.com>
2021-12-20 18:16:03 +0800

    a

---
+++ zwj-sharding/src/main/java/com/dyzwj/zwjsharding/ZwjShardingApplication.java
@@ -0,0 +1,13 @@
+package com.dyzwj.zwjsharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwjShardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZwjShardingApplication.class, args);
+    }
+
+}
---
+++ zwj-sharding/src/test/java/com/dyzwj/zwjsharding/ZwjShardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyzwj.zwjsharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwjShardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
